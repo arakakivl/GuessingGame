2a9c4cdc259b6106970d02c8e061c71258e9ebe6
arakakivl <arakakivl@proton.me>
2023-10-15 02:13:02 -0300

    improving code

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
-use rand::Rng;
 use std::io;
+use std::io::Write;
+use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    // Generating random number
-    let random_number = rand::thread_rng().gen_range(1..=100);
-    println!("Shhh... the answer is {random_number}\n");
-
-    // Game introduction
-    println!("Welcome to the \"Guessing Game\", game!");
-    println!("In order to achieve the victory, you need to guess a number between 1 and 100!");
+    // Generating random number.
+    let secret = rand::thread_rng().gen_range(0..=100);
+    
+    println!("Shh... secret is {secret}.");
     println!();
 
-    // Repeatedly Getting input
+    // Game presentation.
+    println!("Welcome to the \"Guessing Game\", game.");
+    
+    print!("Tell me your name: ");
+    io::stdout().flush().expect("Failed to flush.");
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read line.");
+    
+    println!();
+
+    let name = name.trim();
+    println!("You're welcome, {name}.");
+
+    // Getting answer
     loop {
-        println!("So, your guess is...");
+        println!("What is your answer?");
+        
+        let mut answer = String::new();
+        io::stdin().read_line(&mut answer).expect("Failed to get user answer.");
 
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to get user guess.");
+        let answer: u32 = answer.trim().parse().expect("Failed to parse user answer.");
+        println!();
 
-        // Shadowing guess into u32 data type.
-        let guess: u32 = match guess.trim().parse() {
-            Ok(n) => n,
-            Err(_) => {
-                println!("\nInvalid input. Try again.");
-                continue;
-            }
-        };
-
-        // Comparing guess        
-        match guess.cmp(&random_number) {
+        match answer.cmp(&secret) {
             Ordering::Equal => {
-                println!();
-                println!("Congratulations! You won the game!");
-                
+                println!("You won!");
                 break;
             },
-            Ordering::Greater => println!("Too big!\n"),
-            Ordering::Less => println!("Too small!\n")
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too low!")
         }
+
+        println!();
     }
 }
